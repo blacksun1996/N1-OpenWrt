b09f2f714645b1ed88a7d6f24806a8d3e50d45cf
blacksu <sunye1996517@gmail.com>
2026-06-07 10:47:06 +0800

    更新 N1 固件配置，替换 OpenClash 为 Nikki 应用并调整相关包的安装步骤

--- N1/diy.sh
+++ N1/diy.sh
@@ -2,12 +2,11 @@
 
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
-git clone https://github.com/vernesong/OpenClash.git --depth=1 clone/openclash
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git --depth=1 package/nikki
 
 # Update packages
-rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-amlogic
 cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
-cp -rf clone/openclash/luci-app-openclash feeds/luci/applications/
 
 # Clean packages
 rm -rf clone
